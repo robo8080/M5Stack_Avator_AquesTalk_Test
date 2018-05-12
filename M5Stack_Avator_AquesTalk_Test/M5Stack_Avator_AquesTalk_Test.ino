bd14ad892ad63cee97499b345c35e2934a94172a
robo8080 <motm_rbt@yahoo.co.jp>
2018-05-12 21:42:26 +0900

    initial commit

---
+++ M5Stack_Avator_AquesTalk_Test/M5Stack_Avator_AquesTalk_Test.ino
@@ -0,0 +1,118 @@
+#include <M5Stack.h>
+#include "avator.h"
+//#include "const.h"
+#include "AquesTalkTTS.h"
+
+Avator *avator;
+int count = 0;
+float f = 0;
+float last = 0;
+
+void breath(void *args)
+{
+  int c = 0;
+  for(;;)
+  {
+    c = c + 1 % 100;
+    float f = sin(c * 2 * PI / 100.0);
+    avator->setBreath(f);
+    delay(33);
+  }
+}
+void drawLoop(void *args)
+{
+  for(;;)
+  {
+    int level = TTS.getLevel();
+    float f = level / 12000.0;
+    float open = min(1.0, last + f / 2.0);
+    last = f;
+//    avator->setMouthOpen(1.0);
+    avator->setMouthOpen(open);
+    avator->draw();
+    delay(33);
+//    delay(100);
+  }
+}
+
+void saccade(void *args)
+{
+  for(;;)
+  {
+    float vertical = (float)rand()/(float)(RAND_MAX / 2) - 1;
+    float horizontal = (float)rand()/(float)(RAND_MAX / 2) - 1;
+    avator->setGaze(vertical, horizontal);
+    delay(500 + 100 * random(20));
+  }
+}
+
+void blink(void *args)
+{
+  for(;;)
+  {
+    avator->setEyeOpen(1);
+    delay(2500 + 100 * random(20));
+    avator->setEyeOpen(0);
+    delay(300 + 10 * random(20));
+  }
+}
+
+void setup()
+{
+  int iret;
+//  iret = TTS.create(AQUESTALK_KEY);
+  iret = TTS.create(NULL);
+  M5.begin();
+  M5.Lcd.setBrightness(30);
+  avator = new Avator();
+  xTaskCreatePinnedToCore(
+                    drawLoop,     /* Function to implement the task */
+                    "drawLoop",   /* Name of the task */
+                    4096,      /* Stack size in words */
+                    NULL,      /* Task input parameter */
+                    1,         /* Priority of the task */
+                    NULL,      /* Task handle. */
+                    1);        /* Core where the task should run */
+  xTaskCreatePinnedToCore(
+                    saccade,     /* Function to implement the task */
+                    "saccade",   /* Name of the task */
+                    4096,      /* Stack size in words */
+                    NULL,      /* Task input parameter */
+                    3,         /* Priority of the task */
+                    NULL,      /* Task handle. */
+                    1);        /* Core where the task should run */
+  xTaskCreatePinnedToCore(
+                    breath,     /* Function to implement the task */
+                    "breath",   /* Name of the task */
+                    4096,      /* Stack size in words */
+                    NULL,      /* Task input parameter */
+                    2,         /* Priority of the task */
+                    NULL,      /* Task handle. */
+                    1);        /* Core where the task should run */
+  xTaskCreatePinnedToCore(
+                    blink,     /* Function to implement the task */
+                    "blink",   /* Name of the task */
+                    4096,      /* Stack size in words */
+                    NULL,      /* Task input parameter */
+                    2,         /* Priority of the task */
+                    NULL,      /* Task handle. */
+                    1);        /* Core where the task should run */
+}
+
+void loop()
+{
+  M5.update();
+  if (M5.BtnA.wasPressed())
+  {
+    TTS.play("emufaibu,sutakku,tanosiiyo", 90);
+  }
+  if (M5.BtnB.wasPressed())
+  {
+    TTS.play("nama'mugi/nama'gome/namata'mago.",130);
+  }
+  if (M5.BtnC.wasPressed())
+  {
+    TTS.play("yukkuri,siteittene?", 50);
+  }
+  delay(125);
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # M5Stack_Avator_AquesTalk_Test
-M5Stack_Avator_AquesTalk_Test
+M5StackでAvator表示とAquesTalkのテスト
+
+
+meganetaaanさんのm5stack-avatorをベースにさせていただきました。<br><br>
+オリジナルはこちら。<br>
+An M5Stack library for rendering avatar faces <https://github.com/meganetaaan/m5stack-avator><br>
+
+---
+### 必要な物 ###
+* [M5Stack](http://www.m5stack.com/ "Title") (Grayで動作確認をしました。)<br>
+* Arduino IDE (1.8.5で動作確認をしました。)<br>
+* [Arduino core for the ESP32](https://github.com/espressif/arduino-esp32 "Title")
+* [M5Stack Library](https://github.com/m5stack/M5Stack.git "Title")
+* [AquesTalk pico for ESP32](http://blog-yama.a-quest.com/?eid=970188 "Title")<br><br>
